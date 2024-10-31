daa5f4c606a708761a2405a48db4bb95883ced0f
NakoTurk <filmoloji89@gmail.com>
2024-11-01 02:40:18 +0300

    Update nodescript.sh

--- nodescript.sh
+++ nodescript.sh
@@ -59,7 +59,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v2.1.2
+APP_VERSION=v2.3.1
 git checkout tags/$APP_VERSION -b $APP_VERSION
 make install
 celestia-appd version && sleep 3
@@ -171,7 +171,7 @@ cd $HOME
 rm -rf celestia-node 
 git clone https://github.com/celestiaorg/celestia-node.git 
 cd celestia-node/ 
-git checkout tags/v0.16.0
+git checkout tags/v0.17.2
 make build 
 make install 
 make cel-key 
@@ -230,7 +230,7 @@ cd $HOME
 rm -rf celestia-node 
 git clone https://github.com/celestiaorg/celestia-node.git 
 cd celestia-node/ 
-git checkout tags/v0.16.0 
+git checkout tags/v0.17.2 
 make build 
 make install 
 make cel-key 
@@ -240,7 +240,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v2.1.2
+APP_VERSION=v2.3.1
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 
@@ -300,7 +300,7 @@ cd $HOME
 rm -rf celestia-node 
 git clone https://github.com/celestiaorg/celestia-node.git 
 cd celestia-node/ 
-git checkout tags/v0.16.0 
+git checkout tags/v0.17.2 
 make build 
 make install 
 make cel-key 
